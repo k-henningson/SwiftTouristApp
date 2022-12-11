b633d0e41cb4e0c0d87c5380df6bcbef245632fe
Kyra Henningson <kyrahenningson@hotmail.com>
2022-12-11 11:17:36 -0800

    Initial Commit

---
+++ VanTourism/VanTourismApp.swift
@@ -0,0 +1,17 @@
+//
+//  VanTourismApp.swift
+//  VanTourism
+//
+//  Created by Kyra Henningson on 2022-12-11.
+//
+
+import SwiftUI
+
+@main
+struct VanTourismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
